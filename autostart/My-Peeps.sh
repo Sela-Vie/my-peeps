2995e6a152413033ba0d9648f1d0dce54d1bb332
Giulio Yongco <yongcogiulio125@gmail.com>
2025-11-07 16:59:54 +0800

    change from template

--- autostart/My-Peeps.sh
+++ autostart/My-Peeps.sh
@@ -3,7 +3,7 @@ set -e  # stop on first error
 LOG="$HOME/git/My-Peeps/autostart/auto-cibo.log"
 exec > >(tee -a "$LOG") 2>&1
 
-tmux new -d -s cibo || true # wont stop if tmux session notif already exists
+tmux new -d -s peeps_backend || true # wont stop if tmux session notif already exists
 tmux send-keys -t cibo "cd ~"  C-m
 tmux send-keys -t cibo "source venv_api/bin/activate"  C-m
 tmux send-keys -t cibo "cd ~/git/My-Peeps/backend" C-m
