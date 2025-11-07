28481df898602e83c9afb5f6525f83ab3a9ddf5b
Giulio Yongco <yongcogiulio125@gmail.com>
2025-11-07 16:22:43 +0800

    auto start

---
+++ autostart/My-Peeps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e  # stop on first error
+LOG="$HOME/git/My-Peeps/autostart/auto-cibo.log"
+exec > >(tee -a "$LOG") 2>&1
+
+tmux new -d -s cibo || true # wont stop if tmux session notif already exists
+tmux send-keys -t cibo "cd ~"  C-m
+tmux send-keys -t cibo "source venv_api/bin/activate"  C-m
+tmux send-keys -t cibo "cd ~/git/My-Peeps/backend" C-m
+tmux send-keys -t cibo "python3 main.py" C-m
