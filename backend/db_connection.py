fc9b3bec9edef8a32421f0986a3291e8e45a19a2
Sela-Vie <yongcogiulio125@gmail.com>
2026-06-01 23:53:40 +0800

    updated SQL usage

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -23,45 +23,44 @@ class SQLConnection():
 	# ====================================================================
 	
 	def SQL_execute(
-		self,
-		SQL_command:str
-	) -> bool:
+			self,
+			SQL_command:str,
+			params:tuple = ()
+		) -> bool:
 		is_executed_just_fine = False
 
 		try:
 			this_connection:sqlite3.Connection = sqlite3.connect(self.db_file_name)
 			this_cursor:sqlite3.Cursor = this_connection.cursor()
-			this_cursor.execute(textwrap.dedent(SQL_command))
+			this_cursor.execute(
+				textwrap.dedent(SQL_command),
+				params  # tuple or dict
+			)
 			this_connection.commit()
 			this_connection.close()
 			is_executed_just_fine = True
 		except Exception as e:
-			print(e)
+			raise(e)
 
 		return is_executed_just_fine
 
 	def SQL_fetch(
 			self,
-			SQL_command:str
-		) -> bool:
-		ret_val = 0
+			SQL_command:str,
+			params:tuple = ()
+		) -> list:
+		ret_val:list = []
 
 		try:
 			this_connection:sqlite3.Connection = sqlite3.connect(self.db_file_name)
 			this_cursor:sqlite3.Cursor = this_connection.cursor()
-			this_cursor.execute(textwrap.dedent(SQL_command))
-			ret_val:list = this_cursor.fetchall()
+			this_cursor.execute(
+				textwrap.dedent(SQL_command),
+				params  # tuple or dict
+			)
+			ret_val = this_cursor.fetchall()
 			this_connection.close()
 		except Exception as e:
-			print(e)
+			raise(e)
 
 		return ret_val
-
-	# ====================================================================
-	# this thing is just here for the utility
-
-	def SQL_value(self, val) -> str:
-		if val == -1 or val == "NULL":
-			return "NULL"
-		else:
-			return f'"{val}"'
