c9dcc777614a5b06773089f0177088038b53fa3e
Sela-Vie <yongcogiulio125@gmail.com>
2026-06-22 08:27:58 +0800

    renamed to lower case

--- autostart/my-peeps.sh
+++ autostart/my-peeps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e  # stop on first error
-LOG="$HOME/git/My-Peeps/autostart/peeps_backend.log"
+LOG="$HOME/git/my-peeps/autostart/peeps_backend.log"
 exec > >(tee -a "$LOG") 2>&1
 
 tmux new -d -s peeps_backend || true # wont stop if tmux session notif already exists
 tmux send-keys -t peeps_backend "cd ~"  C-m
 tmux send-keys -t peeps_backend "source venv_api/bin/activate"  C-m
-tmux send-keys -t peeps_backend "cd ~/git/My-Peeps/backend" C-m
+tmux send-keys -t peeps_backend "cd ~/git/my-peeps/backend" C-m
 tmux send-keys -t peeps_backend "python3 main.py" C-m
